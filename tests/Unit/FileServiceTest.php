95c81898c52c9bd0aec590187d3b9e8180794748
Huy <huynn@rabiloo.com>
2024-11-06 16:11:01 +0700

    Fix

--- tests/Unit/FileServiceTest.php
+++ tests/Unit/FileServiceTest.php
@@ -6,9 +6,11 @@ use App\Service\FileService;
 use ArgumentCountError;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 use Throwable;
+use TypeError;
 
 class FileServiceTest extends TestCase
 {
@@ -24,9 +26,8 @@ class FileServiceTest extends TestCase
      * A basic unit test example.
      */
     #[DataProvider('provider_store_file')]
-    public function test_store_file_return_full_data_and_file_exist($folder, $fileName): void
+    public function test_store_file_return_full_data_and_file_exist($folder, $file): void
     {
-        $file = UploadedFile::fake()->create($fileName);
         $arrayKeys=['file_url','file_name','file_extension'];
 
         $fileData = $this->fileService->storeFile($file, $folder);
@@ -40,25 +41,32 @@ class FileServiceTest extends TestCase
     public static function provider_store_file()
     {
         return [
-            "image" => ['avatars','avatars.jpg'],
-            "document" => ['documents','document.pdf'],
-            "without folder" => ['','file.docx']
+            "image" => ['avatars',UploadedFile::fake()->create('avatars.jpg')],
+            "document" => ['documents',UploadedFile::fake()->create('document.pdf')],
+            "without folder" => ['',UploadedFile::fake()->create('file.docx')]
         ];
     }
 
-    #[DataProvider('provider_delete_file')]
-    public function test_delete_existing_file($folder, $fileName)
+    public function test_store_file_fail_with_null_file(): void
     {
-        $file = UploadedFile::fake()->create($fileName);
+        $this->expectException(TypeError::class);
+        $folder = '';
+        $file = null;
+
+        $fileData = $this->fileService->storeFile($file, $folder);
+    }
+
+    #[DataProvider('provider_delete_file')]
+    public function test_delete_existing_file($folder, $file)
+    {
         $fileData = $this->fileService->storeFile($file, $folder);
         
         $this->assertTrue($this->fileService->deleteFile($fileData['file_url']));
     }
 
     #[DataProvider('provider_delete_file')]
-    public function test_delete_non_existing_file($folder, $fileName)
+    public function test_delete_non_existing_file($folder, $file)
     {
-        $file = UploadedFile::fake()->create($fileName);
         $fileData = $this->fileService->storeFile($file, $folder);
         $this->fileService->deleteFile($fileData['file_url']);
 
@@ -68,8 +76,8 @@ class FileServiceTest extends TestCase
     public static function provider_delete_file()
     {
         return [
-            ['avatars','avatars.jpg'],
-            ['documents','document.pdf'],
+            ['avatars',UploadedFile::fake()->create('avatars.jpg')],
+            ['documents',UploadedFile::fake()->create('document.pdf')],
         ];
     }
 
