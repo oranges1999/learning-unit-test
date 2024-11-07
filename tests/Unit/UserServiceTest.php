49b883147217ea3994c2116aa6fcc4a6919c35cd
Huy <huynn@rabiloo.com>
2024-11-07 16:26:12 +0700

    Fix

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -7,46 +7,42 @@ use App\Service\User;
 
 class UserServiceTest extends TestCase
 {
-    protected $userData;
+    protected $userData, $user;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->userData  = ['email'=>'huy@test.com'];
+        $this->user = new User($this->userData);
     }
     
     public function test_get_user()
     {
-        $user = new User($this->userData);
-        $this->assertSame($this->userData, $user->getUser());
+        $this->assertSame($this->userData, $this->user->getUser());
     }
 
     public function test_set_password_return_true()
     {
-        $user = new User($this->userData);
-        $result = $user->setPassword('123456');
+        $result = $this->user->setPassword('123456');
         $this->assertTrue($result);
     }
 
     public function test_set_password_return_false()
     {
-        $user = new User($this->userData);
-        $result = $user->setPassword('123');
+        $result = $this->user->setPassword('123');
         $this->assertFalse($result);
     }
 
     public function test_password_exist()
     {
-        $user = new User($this->userData);
-        $user->setPassword('123456');
-        $this->assertTrue(array_key_exists('password', $user->getUser()));
+        $this->user->setPassword('123456');
+        $this->assertTrue(array_key_exists('password', $this->user->getUser()));
     }
 
     public function test_password_is_md5_hashed()
     {
-        $user = new User($this->userData);
-        $user->setPassword('123456');
-        $data= $user->getUser();
+        $this->user->setPassword('123456');
+        $data= $this->user->getUser();
         $expect = md5('123456');
         $this->assertSame($expect,$data['password']);
     }
